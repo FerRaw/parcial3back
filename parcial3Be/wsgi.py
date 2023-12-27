7ac61c458d55a827e2dfbb1e94af56e591f3d395
FerRaw <100798272+FerRaw@users.noreply.github.com>
2023-12-27 22:06:17 +0100

    Update wsgi.py

--- parcial3Be/wsgi.py
+++ parcial3Be/wsgi.py
@@ -10,8 +10,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from mangum import Mangum
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parcial3Be.settings')
 
 application = get_wsgi_application()
-handler = application
+handler = Mangum(django_app)
